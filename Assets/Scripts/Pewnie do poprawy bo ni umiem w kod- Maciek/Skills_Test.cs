5466a9483c533968087d20e590085852f0ddaf5c
WinterGeneral <rmk5@op.pl>
2024-03-20 15:32:31 +0100

    Skill_test. Ni umiem można zmienić

--- Assets/Scripts/Pewnie do poprawy bo ni umiem w kod- Maciek/Skills_Test.cs
+++ Assets/Scripts/Pewnie do poprawy bo ni umiem w kod- Maciek/Skills_Test.cs
@@ -9,6 +9,8 @@ public class Skills_Test : MonoBehaviour
     int hp;
     [SerializeField]
     int dmg;
+    [SerializeField]
+    int energia;
     //Ikonki??? Nwm pwenie da sie lepiej
     [SerializeField]
     GameObject Ikona_Atk;
@@ -22,26 +24,37 @@ public class Skills_Test : MonoBehaviour
     GameObject Ikona_ASp;
 
     //Dane niezmienne
-    int def_Add = 5;
-    int block = 7;
+    int hp_Wroga_TestOnly = 10;
+    int atak_Wroga_TestOnly= 4;
+    int def_Add = 2;
+    int block = 3;
     //Defensywne
     public void Defend()
     {
         hp = hp + def_Add;
+        energia -= 2;
+        
     }
 
     public void Block_Atk()
     {
-
+        hp = hp - atak_Wroga_TestOnly + block;
+        energia -= 3;
     }
+    //po huj to robie i tak nie umiem
     public void Doge_Atk() 
     {
-    
+        energia -= 1;
+        int los = Random.Range(1,10);
+        if (los % 2 == 1)
+        {
+            hp = hp - atak_Wroga_TestOnly;
+        }
     }
     //Ofensywne
     public void Atack()
     {
-
+        hp_Wroga_TestOnly = hp_Wroga_TestOnly - dmg;
     }
 
     // Start is called before the first frame update
