08e8909015892fb728e8e44e95c88c482aa2a827
WinterGeneral <rmk5@op.pl>
2024-03-20 15:17:13 +0100

    Dodane Materiały do testów + testowy skrypt skili
    Skille jeszcze w budowie. Dodam w kolejnym

---
+++ Assets/Scripts/Pewnie do poprawy bo ni umiem w kod- Maciek/Skills_Test.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skills_Test : MonoBehaviour
+{
+    //Dane zależne od postaci
+    [SerializeField]
+    int hp;
+    [SerializeField]
+    int dmg;
+    //Ikonki??? Nwm pwenie da sie lepiej
+    [SerializeField]
+    GameObject Ikona_Atk;
+    [SerializeField]
+    GameObject Ikona_Def;
+    [SerializeField]
+    GameObject Ikona_Dge;
+    [SerializeField]
+    GameObject Ikona_Bck;
+    [SerializeField]
+    GameObject Ikona_ASp;
+
+    //Dane niezmienne
+    int def_Add = 5;
+    int block = 7;
+    //Defensywne
+    public void Defend()
+    {
+        hp = hp + def_Add;
+    }
+
+    public void Block_Atk()
+    {
+
+    }
+    public void Doge_Atk() 
+    {
+    
+    }
+    //Ofensywne
+    public void Atack()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
